80baf09e33ffd95100773b2341713bc23a1ad94e
Anubrata Guin <141846655+AnuGuin@users.noreply.github.com>
2025-07-29 21:34:28 +0530

    Create Sol.cpp

---
+++ Introductory_Problems/Q3/Sol.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <string>
+ 
+using namespace std;
+ 
+int longestRepetition(const string& dna) {
+    int maxRep = 0, currRep = 1;
+    for (int i = 1; i < dna.size(); i++) {
+        if (dna[i] == dna[i - 1]) {
+            currRep++;
+        } else {
+            maxRep = max(maxRep, currRep);
+            currRep = 1;
+        }
+    }
+    return max(maxRep, currRep);
+}
+ 
+int main() {
+    string dna;
+    cin >> dna;
+    cout << longestRepetition(dna) << endl;
+    return 0;
+}
