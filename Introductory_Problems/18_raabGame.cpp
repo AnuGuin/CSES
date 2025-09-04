c331812e1a466ca6138e09a8916041452b21db28
Anubrata Guin <anu.guin.01@gmail.com>
2025-09-04 15:03:14 +0530

    q 21 added

--- Introductory_Problems/18_raabGame.cpp
+++ Introductory_Problems/18_raabGame.cpp
@@ -1,55 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool test_cases() {
+    int n, a, b; // n = no of cards, a = alice wins, b = bob wins
+    cin >> n >> a >> b;
 
-bool test_cases(){
-	int n, a, b; //n = no of cards, a = alice wins, b = bob wins
-	cin >> n >> a >> b;
-	// first consideration is no of ties
-	int ties = n-a-b;
-	if(ties < 0){
-		return false;
-	}
-	n -= ties;
-	if(n < 0) {
-		return false;
-	} 
-	assert(a+b == n); // debugging
-	if(n>=1 && (a==n || b==n)){
-		return false;
-	}
-	cout << "YES \n";
+    int ties = n - a - b;
+    if (ties < 0) return false;
 
-	//for alice accepting tied cases
-	for(int i = 1; i < n; i++){
-		cout << i << " ";
-	}
+    n -= ties;
+    if (n < 0) return false;
 
-	for(int i = n+1; i < n+ties; i++){
-		cout << i << " ";
-	}
-	cout << "\n";
-	// for bob
-	for(int i = 0; i < n; i++){
-		int x = i+a;
-		if(x > n){
-			x -= n;
-		}
-		cout << x << " ";
-	}
-	for(int i = n+1; i < n+ties; i++){
-		cout << i << " ";
-	}
-	return true;
+    if (a + b != n) return false;
+
+    if (n >= 1 && (a == n || b == n)) return false;
+
+    cout << "YES\n";
+
+    // Alice
+    for (int i = 1; i <= n; i++) {
+        cout << i << " ";
+    }
+    for (int i = n + 1; i <= n + ties; i++) {
+        cout << i << " ";
+    }
+    cout << "\n";
+
+    // Bob
+    for (int i = 0; i < n; i++) {
+        int x = i + a;
+        if (x >= n) x -= n;
+        cout << x + 1 << " ";
+    }
+    for (int i = n + 1; i <= n + ties; i++) {
+        cout << i << " ";
+    }
+    cout << "\n";
+
+    return true;
 }
 
 int main() {
-	int t;
-	cin >> t;
-	while(t--){
-		if(!test_cases){
-			cout << "NO\n";
-		}
-	}
-   
+    int t;
+    cin >> t;
+    while (t--) {
+        if (!test_cases()) {
+            cout << "NO\n";
+        }
+    }
 }
--- file.cpp
+++ file.cpp
@@ -1,9 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long answer = 0;
-
-
 int main() {
-    
-}
+    int n, m; // n = row or height, m = col or width
+    cin >> n >> m;
+   vector<string> grid(n);
+    for(string &s: grid){
+        cin >> s;
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if((i+j) % 2 == 0){
+                grid[i][j] = (grid[i][j] == 'A' ? 'C' : 'A');
+            } else {
+                grid[i][j] = (grid[i][j] == 'B' ? 'D' : 'B');
+            }    
+
+        }
+    cout << grid[i] << endl;
+    }
+
+ }
+
